35d8b1f3bde64cac7aecb7fa1100ad6282b22f59
Cesar Done <cesardone@live.com>
2020-05-28 17:56:32 -0400

    Completed loopy loop

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,9 +2,28 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  let node = linkedlist.head;
+  const nodeArray = [];
 
+  while (node) {
+    if (nodeArray.includes(node.value)) return true;
+    else {
+      nodeArray.push(node.value);
+    }
+    node = node.next;
+  }
+  return false;
 };
 
+//Sudo Code
+/* 
+    1. Traverse thru each of the nodes in LL
+    2. While traversing, check if node.value exists in array, using Array.includes
+    3. If not, add into array
+        - if yes, return true
+    4. If LL ends in null, then return false
+
+*/
 
 /*
 EXTRA CREDIT:
@@ -14,4 +33,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
